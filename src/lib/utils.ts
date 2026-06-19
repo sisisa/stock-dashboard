f772b6444bbf67b7f5d25e8d8ce1571c4c1f9967
sisisa <105726977+sisisa@users.noreply.github.com>
2026-06-19 13:55:43 +0900

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+/**
+ * Tailwind CSS のクラス名を結合し、
+ * 重複・競合するクラスを自動で整理する
+ */
+export function cn(...classNames: ClassValue[]) {
+  // clsxで条件付きクラスを組み立てる
+  const mergedClasses = clsx(classNames);
+
+  // Tailwindの競合クラスを解決する
+  return twMerge(mergedClasses);
 }
